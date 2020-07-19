fd123832ffa157509648f38442b57ee63dde91b4
Manpreet Singh <er.manpreet68@gmail.com>
2020-07-19 23:18:43 +0530

    Changes in hero search component

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
 import { Observable, Subject } from 'rxjs';
-
-import {
-   debounceTime, distinctUntilChanged, switchMap
- } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
@@ -14,7 +10,9 @@ import { HeroService } from '../hero.service';
   templateUrl: './hero-search.component.html',
   styleUrls: [ './hero-search.component.css' ]
 })
+
 export class HeroSearchComponent implements OnInit {
+  
   heroes$: Observable<Hero[]>;
   private searchTerms = new Subject<string>();
 
