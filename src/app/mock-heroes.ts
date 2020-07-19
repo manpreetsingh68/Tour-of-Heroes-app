717f892190ab96200e3e95e3e00a257167865720
Manpreet Singh <er.manpreet68@gmail.com>
2020-07-19 19:28:30 +0530

    Initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Spider Man'},
+    {id: 12, name: 'Bat Man'},
+    {id: 13, name: 'Flash'},
+    {id: 14, name: 'Super Man'},
+    {id: 15, name: 'The Hulk'},
+    {id: 16, name: 'Iron Man'},
+    {id: 17, name: 'Black Widow'},
+    {id: 18, name: 'Aqua Man'},
+    {id: 19, name: 'Professor X'},
+    {id: 20, name: 'Doctor Strange'}
+];
